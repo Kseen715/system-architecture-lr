8285f395d98ca1390e15f5bea6c7f93f514f77bb
kseen <korenevdn@gmail.com>
2024-11-16 23:38:29 +0300

    Refactor comments in lab2.asm for clarity and consistency

--- lr2-opcodes/lab2.asm
+++ lr2-opcodes/lab2.asm
@@ -1,23 +1,23 @@
- .386 ; ��� ����������
- .model flat, stdcall ; ������ ������ � ����� ������ �����������
- option casemap: none ; ���������������� � ��������
+ .386 ; Тип процессора 
+ .model flat, stdcall ; Модель памяти и стиль вызова подпрограмм 
+ option casemap: none ; Чувствительность к регистру 
 
- ; --- ����������� ������ � �����, ���������, �����������, 
- ; ����������� ������� � �.�.
+ ; --- Подключение файлов с кодом, макросами, константами,  
+ ; прототипами функций и т.д. 
  include windows.inc
  include kernel32.inc
  include user32.inc
  include msvcrt.inc
 
- ; --- ������������ ���������� ---
+ ; --- Подключаемые библиотеки --- 
  includelib user32.lib
  includelib kernel32.lib
  includelib msvcrt.lib
 
- ; --- ������� ������ ---
+ ; --- Сегмент данных --- 
  .data
 
- ; --- ������� ���� ---
+ ; --- Сегмент кода --- 
  .code
  start:
 	MOV AX, 120h 
@@ -36,8 +36,6 @@
 	MOV EBX, 64h
 	MOV EAX, 78h
 
-	
-
   	push NULL
-  	call ExitProcess ; ����� �� ���������
+  	call ExitProcess ; Выход из программы 
  end start
