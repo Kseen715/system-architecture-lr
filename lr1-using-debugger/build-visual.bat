57a17040add5f31c83b75e057bc8db8e715ff8ad
kseen <korenevdn@gmail.com>
2024-11-16 23:00:52 +0300

    Add build scripts and assembly source for lab1 project

---
+++ lr1-using-debugger/build-visual.bat
@@ -0,0 +1,11 @@
+@REM set var MASM32 path:
+set MASM32=d:\projects\masm32
+set FILE=lab1-visual
+
+del %FILE%.exe
+%MASM32%\bin\ml /c /coff /I "%MASM32%\include" %FILE%.asm
+%MASM32%\bin\ml /c /coff /I "%MASM32%\include" %FILE%.asm
+%MASM32%\bin\link /SUBSYSTEM:CONSOLE /LIBPATH:%MASM32%\lib %FILE%.obj
+del %FILE%.obj
+%FILE%.exe
+pause
