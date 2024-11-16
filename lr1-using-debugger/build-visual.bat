0ac6c723e60dea84221280acb9c8132e47ecc8f6
kseen <korenevdn@gmail.com>
2024-11-16 23:30:11 +0300

    Enhance build scripts to create a dedicated build folder and streamline the compilation process

--- lr1-using-debugger/build-visual.bat
+++ lr1-using-debugger/build-visual.bat
@@ -1,11 +1,29 @@
 @REM set var MASM32 path:
 set MASM32=d:\projects\masm32
 set FILE=lab1-visual
+set BUILD_FOLDER=build
 
-del %FILE%.exe
-%MASM32%\bin\ml /c /coff /I "%MASM32%\include" %FILE%.asm
-%MASM32%\bin\ml /c /coff /I "%MASM32%\include" %FILE%.asm
+@REM if build folder does not exist, create it
+if not exist %BUILD_FOLDER% mkdir %BUILD_FOLDER%
+
+@REM cd to build folder
+cd %BUILD_FOLDER%
+
+@REM remove old exe file
+if exist %FILE%.exe del %FILE%.exe
+
+@REM compile
+%MASM32%\bin\ml /c /coff /I "%MASM32%\include" ../%FILE%.asm
+
+@REM link
 %MASM32%\bin\link /SUBSYSTEM:CONSOLE /LIBPATH:%MASM32%\lib %FILE%.obj
+
+@REM remove obj file
 del %FILE%.obj
+
+@REM run
 %FILE%.exe
+
+@REM cd back
+cd ..
 pause
