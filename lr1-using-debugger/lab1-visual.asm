1acac51e57d468ee8c586de6269b3129b679bc34
kseen <korenevdn@gmail.com>
2024-11-16 23:02:56 +0300

    Refactor assembly code for clarity and consistency

--- lr1-using-debugger/lab1-visual.asm
+++ lr1-using-debugger/lab1-visual.asm
@@ -1,109 +1,106 @@
- .386 ; ��� ����������
- .model flat, stdcall ; ������ ������ � ����� ������ �����������
- option casemap: none ; ���������������� � ��������
-
- ; --- ����������� ������ � �����, ���������, �����������, 
- ; ����������� ������� � �.�.
- include windows.inc
- include kernel32.inc
- include user32.inc
- include msvcrt.inc
-
- ; --- ������������ ���������� ---
- includelib user32.lib
- includelib kernel32.lib
- includelib msvcrt.lib
-
- ; --- ������� ������ ---
- .data 
-	strd DB "Division", 0
-	a DD 10500000h, 1200h
-	b DD 10000
-	m DD ?
-	mas DW 8 DUP(1)
-	ten DT 30000, -30000
-	f DQ 1, 1.0, -1.0
-	h DF -3, -2, -1, 0, 1, 2, 3, 4, 40000h, 40000
-
-	format DB 8 DUP ("%s", 9, "%p", 9 , "%p", 9, "%d", 13, 10), 0
-	header DB "var", 9, "addr", 9,  9, "end", 9, 9, "size", 13, 10,
-		"---------------------------------------------------------------------", 
-		13, 10, 0
-		
-	arg1_name DB "strd", 0
-	arg2_name DB "a", 0
-	arg3_name DB "b", 0
-	arg4_name DB "m", 0
-	arg5_name DB "mas", 0
-	arg6_name DB "ten", 0
-	arg7_name DB "f", 0
-	arg8_name DB "h", 0
-	null_name DB " ", 0
-
-	buffer DB 1000 DUP (?)
-
- ; --- ������� ���� ---
- .code
- start:
-	push offset header
-	push offset buffer
-	call crt_sprintf
-	add esp, 3*4
-
-	push (offset format) - offset h
-	push ((offset format) - 1)
-	push offset h
-	push offset arg8_name
-
-	push (offset h) - offset f
-	push ((offset h) - 1)
-	push offset f
-	push offset arg7_name
-
-	push (offset f) - offset ten
-	push (offset f - 1)
-	push offset ten
-	push offset arg6_name
-
-	push (offset ten) - offset mas
-	push ((offset ten) - 1)
-	push offset mas
-	push offset arg5_name
-
-	push (offset mas) - offset m
-	push ((offset mas) - 1)
-	push offset m
-	push offset arg4_name
-
-	push (offset m) - offset b
-	push ((offset m) - 1)
-	push offset b
-	push offset arg3_name
-
-	push (offset b) - offset a
-	push ((offset b) - 1)
-	push offset a
-	push offset arg2_name
-
-	push (offset a) - offset strd 
-	push ((offset a) - 1)
-	push offset strd
-	push offset arg1_name
-
-	push offset format
-	push offset buffer[92]
-	call crt_sprintf
-	add esp, 31*4
-
-
-	push NULL
-	push NULL
-	push offset buffer
-	push NULL
-	call MessageBoxA
-	add esp, 5*4
-
-
-  	push NULL
-  	call ExitProcess ; ����� �� ���������
+.386 ; Тип процессора 
+ .model flat, stdcall ; Модель памяти и стиль вызова подпрограмм 
+ option casemap: none ; Чувствительность к регистру 
+ 
+ ; --- Подключение файлов с кодом, макросами, константами,  
+ ; прототипами функций и т.д. 
+ include windows.inc 
+ include kernel32.inc 
+ include user32.inc 
+ include msvcrt.inc 
+ 
+ ; --- Подключаемые библиотеки --- 
+ includelib user32.lib 
+ includelib kernel32.lib 
+ includelib msvcrt.lib 
+ 
+ ; --- Сегмент данных --- 
+ .data  
+    strd DB "Division", 0 
+    a DD 10500000h, 1200h 
+    b DD 10000 
+    m DD ? 
+    mas DW 8 DUP(1) 
+    ten DT 30000, -30000 
+    f DQ 1, 1.0, -1.0 
+    h DF -3, -2, -1, 0, 1, 2, 3, 4, 40000h, 40000 
+ 
+    format DB 8 DUP ("%s", 9, "%p", 9 , "%p", 9, "%d", 13, 10), 0 
+    header DB "var", 9, "addr", 9,  9, "end", 9, 9, "size", 13, 10, 
+        "---------------------------------------------------------------------",  
+  13, 10, 0 
+         
+    arg1_name DB "strd", 0 
+    arg2_name DB "a", 0 
+    arg3_name DB "b", 0 
+    arg4_name DB "m", 0 
+    arg5_name DB "mas", 0 
+    arg6_name DB "ten", 0 
+    arg7_name DB "f", 0 
+    arg8_name DB "h", 0 
+ 
+    buffer DB 1000 DUP (?) 
+ 
+ ; --- Сегмент кода --- 
+ .code 
+ start: 
+    push offset header 
+    push offset buffer 
+    call crt_sprintf 
+    add esp, 3*4 
+ 
+    push (offset format) - offset h 
+    push ((offset format) - 1) 
+    push offset h 
+    push offset arg8_name 
+ 
+    push (offset h) - offset f 
+    push ((offset h) - 1) 
+    push offset f 
+    push offset arg7_name 
+ 
+    push (offset f) - offset ten 
+    push (offset f - 1) 
+    push offset ten 
+    push offset arg6_name 
+ 
+    push (offset ten) - offset mas 
+    push ((offset ten) - 1) 
+    push offset mas 
+    push offset arg5_name 
+ 
+    push (offset mas) - offset m 
+    push ((offset mas) - 1) 
+    push offset m 
+    push offset arg4_name 
+ 
+    push (offset m) - offset b 
+    push ((offset m) - 1) 
+    push offset b 
+    push offset arg3_name 
+ 
+    push (offset b) - offset a 
+    push ((offset b) - 1) 
+    push offset a 
+    push offset arg2_name 
+ 
+    push (offset a) - offset strd  
+    push ((offset a) - 1) 
+    push offset strd 
+    push offset arg1_name 
+ 
+    push offset format 
+    push offset buffer[92] 
+    call crt_sprintf 
+    add esp, 31*4 
+ 
+    push NULL 
+    push NULL 
+    push offset buffer 
+    push NULL 
+    call MessageBoxA 
+    add esp, 5*4 
+ 
+    push NULL 
+    call ExitProcess ; Выход из программы 
  end start
--- lr1-using-debugger/lab1.asm
+++ lr1-using-debugger/lab1.asm
@@ -1,40 +1,41 @@
- .386 ; ��� ����������
- .model flat, stdcall ; ������ ������ � ����� ������ �����������
- option casemap: none ; ���������������� � ��������
-
- ; --- ����������� ������ � �����, ���������, �����������, 
- ; ����������� ������� � �.�.
- include windows.inc
- include kernel32.inc
- include user32.inc
- include msvcrt.inc
-
- ; --- ������������ ���������� ---
- includelib user32.lib
- includelib kernel32.lib
- includelib msvcrt.lib
-
- ; --- ������� ������ ---
- .data 
-	strd DB "Division", 0
-	a DD 10500000h, 1200h
-	b DD 10000
-	m DD ?
-	mas DW 8 DUP(1)
-	ten DT 30000, -30000
-	f DQ 1, 1.0, -1.0
-	h DF -3, -2, -1, 0, 1, 2, 3, 4, 40000h, 40000
-
- ; --- ������� ���� ---
- .code
- start:
-	MOV EDX, a[4]   ; EDX = a[4] = 0x1200
-	MOV EAX, a[0]	; EAX = a[0] = 0x10500000
-	DIV b			; EAX = EAX / b, EDX = EAX % b
-	MOV m, EDX		; m = 0x120010500000 mod b
-	ADD EAX, m		; EAX = EAX + m
-	IMUL EAX, 3  	; EAX = EAX * 3
-
-  	push NULL
-  	call ExitProcess ; ����� �� ���������
+.386 ; Тип процессора 
+ .model flat, stdcall ; Модель памяти и стиль вызова подпрограмм 
+ option casemap: none ; Чувствительность к регистру 
+ 
+ ; --- Подключение файлов с кодом, макросами, константами,  
+ ; прототипами функций и т.д. 
+ include windows.inc 
+ include kernel32.inc 
+ include user32.inc 
+ include msvcrt.inc 
+ 
+ ; --- Подключаемые библиотеки --- 
+ includelib user32.lib 
+ includelib kernel32.lib 
+ includelib msvcrt.lib 
+ 
+ ; --- Сегмент данных --- 
+ .data  
+    strd DB "Division", 0 
+    a DD 10500000h, 1200h 
+    b DD 10000 
+    m DD ? 
+    mas DW 8 DUP(1) 
+    ten DT 30000, -30000 
+    f DQ 1, 1.0, -1.0 
+    h DF -3, -2, -1, 0, 1, 2, 3, 4, 40000h, 40000 
+ 
+ ; --- Сегмент кода --- 
+ .code 
+ start: 
+    MOV EDX, a[4]   ; EDX = a[4] = 0x1200 
+    MOV EAX, a[0]   ; EAX = a[0] = 0x10500000 
+    DIV b           ; EAX = EAX / b 
+    MOV m, EDX      ; m = 0x120010500000 mod b 
+ 
+    ADD EAX, m      ; EAX = EAX + m 
+    IMUL EAX, 3     ; EAX = EAX * 3 
+ 
+    push NULL 
+    call ExitProcess ; Выход из программы 
  end start
